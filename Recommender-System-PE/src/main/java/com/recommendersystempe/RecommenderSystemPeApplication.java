c36b58940edaa85ba4cc1b55d3207da36577b438
Douglas Fragoso <douglas.iff@gmail.com>
2025-02-05 12:47:14 -0300

    docs: spring initializr

---
+++ Recommender-System-PE/src/main/java/com/recommendersystempe/RecommenderSystemPeApplication.java
@@ -0,0 +1,13 @@
+package com.recommendersystempe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderSystemPeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommenderSystemPeApplication.class, args);
+	}
+
+}
---
+++ Recommender-System-PE/src/test/java/com/recommendersystempe/RecommenderSystemPeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommendersystempe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderSystemPeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
